b01830893d63642d3a6c5c2b2d4d2326ded909d0
Haithemkhelfa <91703061+Haithemkhelfa@users.noreply.github.com>
2022-01-30 23:20:27 +0100

    changement code

--- Projet_Data_Engineering.sh
+++ Projet_Data_Engineering.sh
@@ -14,8 +14,6 @@ echo "FROM python" > tempdir/Dockerfile
 echo "RUN pip3 install pandas" >> tempdir/Dockerfile
 echo "RUN pip3 install json" >> tempdir/Dockerfile
 echo "RUN pip3 install re" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./my_data /home/myapp/my_data/" >> tempdir/Dockerfile
 echo "COPY Projet_Data_Engineering.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/Projet_Data_Engineering.py" >> tempdir/Dockerfile
